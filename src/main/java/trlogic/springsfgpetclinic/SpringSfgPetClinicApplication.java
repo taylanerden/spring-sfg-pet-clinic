b268cd504e642f9a956f9ed39435f4e78936bb94
taylanerden <taylan.erden@trlogic.com>
2018-10-13 22:29:00 +0300

    VERSION 0.1

---
+++ src/main/java/trlogic/springsfgpetclinic/SpringSfgPetClinicApplication.java
@@ -0,0 +1,12 @@
+package trlogic.springsfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSfgPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSfgPetClinicApplication.class, args);
+    }
+}
---
+++ src/test/java/trlogic/springsfgpetclinic/SpringSfgPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package trlogic.springsfgpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringSfgPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
